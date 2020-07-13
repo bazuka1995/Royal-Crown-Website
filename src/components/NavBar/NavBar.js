3c62b1b4f9a14303934dc875f40b3bc5f30ebf38
Dov Royal <dov.royal@gmail.com>
2020-07-13 13:26:59 +1000

    Fixed background images and added in logo
    Fixed the background images to make sure they are behind all the other elements.
    Added in hover elements into the navbar.
    Added in the company name into the logo.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,44 @@
 import React from "react";
 import classes from "./NavBar.module.css";
-import logo from "./../../assets/logo.png"
+import logo from "./../../assets/logo.png";
 
 const navbar = (props) => {
   return (
     <div className={classes.NavBar}>
       <ul className={classes.NavMenu}>
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">About Us</a>
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          About Us
+        </a>
 
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">Commercial</a>
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          Commercial
+        </a>
 
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">Industrial</a>
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          Industrial
+        </a>
 
-        <img src={logo} alt="Royal Crown Logo" width="50" height="50"/>
+        <img
+          className={classes.Logo}
+          src={logo}
+          alt="Royal Crown Logo"
+          width="50"
+          height="50"
+        />
 
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">Residential</a>
+        <p className={classes.Name}>Royal Crown Group</p>
 
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">Tenants</a>
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          Residential
+        </a>
 
-        <a className={classes.NavItem} href="http://royalcrown.com.au/">Contact Us</a>
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          Tenants
+        </a>
+
+        <a className={classes.NavItem} href="http://royalcrown.com.au/">
+          Contact Us
+        </a>
       </ul>
     </div>
   );
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.module.css';
+import classes from './App.module.css';
 import NavBar from '../components/NavBar/NavBar'
 import Background from '../components/Background/Background';
 
 function App() {
   return (
-    <div className="App">
+    <div className={classes.App}>
       <Background />
       <NavBar />
     </div>
